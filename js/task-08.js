58cbbc0ff6592699044d9452c68658037614f36d
ArtSatanov <artsatanov@gmail.com>
2023-07-29 02:15:56 +0300

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+
+
+form.addEventListener('submit', formSubmit); 
+
+function formSubmit(event) {
+   event.preventDefault();
+
+   const { elements: { email, password } } = event.currentTarget;
+
+   if (email.value === '' || password.value === '') {
+      return alert('Please fill out all empty fields');
+   }
+   
+   // const formData = new FormData(event.currentTarget);
+   // console.log(formData);
+
+   const formData = {email: email.value, password: password.value};
+   console.log(formData);
+   event.currentTarget.reset();
+};
